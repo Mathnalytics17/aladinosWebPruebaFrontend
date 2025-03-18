5135d2635a35e36ad1a08ec5092063463d152bb6
Luis <luisjose0317@gmail.com>
2025-03-18 00:32:17 -0500

    modificaciones en fecha nacimiento,campo email y autoguradado2

--- pages/formularioGoogleSheets/index.js
+++ pages/formularioGoogleSheets/index.js
@@ -153,7 +153,7 @@ export default function FormularioGoogleSheets() {
         nombre_asterisco:data.nombre + ' ' +  data.apellidos+ ' '+'-'+ ' '+ 'Socio'
       };
 
-      const response = await axios.post("http://localhost:8000/api/registro/", formattedData);
+      const response = await axios.post("https://api.altasfundacionaladina.org/api/registro/", formattedData);
       console.log("Registro exitoso:", response.data);
       setSuccess(true);
       setError(null);
@@ -296,7 +296,7 @@ const formatDateToDDMMYYYY = (dateString) => {
   
     // Enviar datos al backend
     try {
-      const response = await fetch("http://localhost:8000/api/registro/guardarBorrador/", {
+      const response = await fetch("https://api.altasfundacionaladina.org/api/registro/guardarBorrador/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -414,7 +414,7 @@ const formatDateToDDMMYYYY = (dateString) => {
               if (watch("tipo_identificacion") === "NIF") {
                 try {
                   // Realizar la solicitud al backend
-                  const response = await axios.post("http://localhost:8000/api/validar-dni/", {
+                  const response = await axios.post("https://api.altasfundacionaladina.org/api/validar-dni/", {
                     tipoid:'nif',
                     numero_identificacion: value, // Enviar el valor al backend
                   });
@@ -435,7 +435,7 @@ const formatDateToDDMMYYYY = (dateString) => {
               else if(watch("tipo_identificacion") === "NIE"){
                 try {
                   // Realizar la solicitud al backend
-                  const response = await axios.post("http://localhost:8000/api/validar-dni/", {
+                  const response = await axios.post("https://api.altasfundacionaladina.org/ionaladina.org/api/validar-dni/", {
                     tipoid:'nie',
                     numero_identificacion: value, // Enviar el valor al backend
                   });
@@ -632,7 +632,7 @@ const formatDateToDDMMYYYY = (dateString) => {
       
               try {
                 // Realizar la solicitud al backend para validar el IBAN
-                const response = await axios.post("http://localhost:8000/api/validar_iban/", {
+                const response = await axios.post("https://api.altasfundacionaladina.org/api/validar_iban/", {
                   iban: value, // Enviar el IBAN al backend
                 });
       
